b7b5988d55000caeb069c9819c15a669a0140c43
Daniel <daniel@eightwire.com>
2026-05-03 21:12:06 +1200

    Allow host

--- lib/budgie/cli/server.rb
+++ lib/budgie/cli/server.rb
@@ -6,14 +6,16 @@ module Budgie
       desc "start", "Start the Budgie web UI"
       option :port, type: :numeric, default: 4567, aliases: "-p",
                     desc: "Port to listen on"
+      option :host, type: :string,  default: "localhost", aliases: "-H",
+                    desc: "Host to bind to (use 0.0.0.0 to allow remote access)"
       option :db,   type: :string,  default: Budgie::Database::DEFAULT_PATH, aliases: "-d",
                     desc: "Path to the SQLite database file"
       def start
         Budgie::Server.set(:db_path, options[:db])
         Budgie::Server.set(:port, options[:port])
-        Budgie::Server.set(:bind, "localhost")
+        Budgie::Server.set(:bind, options[:host])
         Budgie::Server.set(:logging, false)
-        say "Starting Budgie web UI at http://localhost:#{options[:port]}"
+        say "Starting Budgie web UI at http://#{options[:host]}:#{options[:port]}"
         say "Database: #{options[:db]}"
         say "Press Ctrl+C to stop."
         Budgie::Server.run!
